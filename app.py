47f007b42344795a25b50be68a68f556d1e97733
Ludovica Baldi <ludovica.baldi@se.com>
2022-12-09 17:08:22 +0000

    User interface V2

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import requests
 import numpy as np
+from wfa.utils.image_viz import summary
+from wfa.utils.get_new_images import get_new_image
 
 st.title(' Website Watching from above ')
 
@@ -32,16 +34,15 @@ params = dict(
     year_1 = year_1,
     year_2 = year_2)
 
-wfa_api_url = '' # url link to be provided
+wfa_api_url = 'https://wfa01-tqv5zy4gla-ew.a.run.app/' # url link to be confirmed
 response = requests.get(wfa_api_url, params=params)
-
 results = response.json()
-#get.image
-#get.image
-classification_summary = results['summary'] # summary to be confirmed
-classification_1 = np.array(results['year_1']) # year_1 to be confirmed
-classification_2 = np.array(results['year_2']) # year_2 to be confirmed
-classification_delta = classification_1 - classification_2
+class_1 = np.array(results['year_1']) # year_1 to be confirmed
+class_2 = np.array(results['year_2']) # year_2 to be confirmed
+
+image_1 = get_new_image(adress, year_1)
+image_2 = get_new_image(adress, year_2)
+changes, summary = summary(class_1, class_2)
 
 col1, col2, col3 = st.columns(3)
 
@@ -49,23 +50,23 @@ st.header(f'Landscape evolution: {year_1} vs. {year_2}')
 
 with col1:
    st.header("Year 1")
-   st.image("https://static.streamlit.io/examples/cat.jpg")
+   st.image(image_1)
 
 with col2:
    st.header("Year 2")
-   st.image("https://static.streamlit.io/examples/dog.jpg")
+   st.image(image_2)
 
 with col3:
    st.header("Landscape evolution")
-   st.image("https://static.streamlit.io/examples/owl.jpg")
+   st.dataframe(summary)
 
 col4, col5, col6 = st.columns(3)
 
 with col4:
-   st.image("https://static.streamlit.io/examples/cat.jpg")
+   st.image(class_1) #np.array: assign RGB code to each class
 
 with col5:
-   st.image("https://static.streamlit.io/examples/dog.jpg")
+   st.image(class_2) #np.array: assign RGB code to each class
 
 with col6:
-   st.image("https://static.streamlit.io/examples/owl.jpg")
+   st.dataframe(changes)
