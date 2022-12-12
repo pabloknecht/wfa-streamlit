ab9be1988377692d6165e8bf8deb19fcb998152c
pablo knecht <pablo.knecht@se.com>
2022-12-12 15:15:50 +0000

    vd

--- app.py
+++ app.py
@@ -40,42 +40,43 @@ wfa_api_url = 'https://wfa01-tqv5zy4gla-ew.a.run.app/watchingfromabove/predictio
 if submitted:
     response = requests.get(wfa_api_url, params=params)
     results = response.json()
+    st.write(results)
 
 
-# st.write(results)
 
 
 
-class_1 = np.array(results['current_year']) # year_1 to be confirmed
-class_2 = np.array(results['historical_year']) # year_2 to be confirmed
 
-image_1 = get_new_image(adress, year_1)
-image_2 = get_new_image(adress, year_2)
-changes, summary = summary(class_1, class_2)
+# class_1 = np.array(results['current_year']) # year_1 to be confirmed
+# class_2 = np.array(results['historical_year']) # year_2 to be confirmed
 
-col1, col2, col3 = st.columns(3)
+# image_1 = get_new_image(adress, year_1)
+# image_2 = get_new_image(adress, year_2)
+# changes, summary = summary(class_1, class_2)
 
-st.header(f'Landscape evolution: {year_1} vs. {year_2}')
+# col1, col2, col3 = st.columns(3)
 
-with col1:
-   st.header("Year 1")
-   st.image(image_1)
+# st.header(f'Landscape evolution: {year_1} vs. {year_2}')
 
-with col2:
-   st.header("Year 2")
-   st.image(image_2)
+# with col1:
+#    st.header("Year 1")
+#    st.image(image_1)
 
-with col3:
-   st.header("Landscape evolution")
-   st.dataframe(summary)
+# with col2:
+#    st.header("Year 2")
+#    st.image(image_2)
 
-col4, col5, col6 = st.columns(3)
+# with col3:
+#    st.header("Landscape evolution")
+#    st.dataframe(summary)
 
-with col4:
-   st.image(class_1) #np.array: assign RGB code to each class
+# col4, col5, col6 = st.columns(3)
 
-with col5:
-   st.image(class_2) #np.array: assign RGB code to each class
+# with col4:
+#    st.image(class_1) #np.array: assign RGB code to each class
 
-with col6:
-   st.dataframe(changes)
+# with col5:
+#    st.image(class_2) #np.array: assign RGB code to each class
+
+# with col6:
+#    st.dataframe(changes)
