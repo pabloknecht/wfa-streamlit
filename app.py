253b7e5cf79366ef482401f8b8290ff271a11b9f
pablo knecht <pablo.knecht@se.com>
2022-12-12 15:06:31 +0000

    vd

--- app.py
+++ app.py
@@ -38,37 +38,42 @@ params = dict(
 wfa_api_url = 'https://wfa01-tqv5zy4gla-ew.a.run.app/' # url link to be confirmed
 response = requests.get(wfa_api_url, params=params)
 results = response.json()
-print(results)
-class_1 = np.array(results['current_year']) # year_1 to be confirmed
-class_2 = np.array(results['historical_year']) # year_2 to be confirmed
 
-image_1 = get_new_image(adress, year_1)
-image_2 = get_new_image(adress, year_2)
-changes, summary = summary(class_1, class_2)
 
-col1, col2, col3 = st.columns(3)
+st.write(results)
 
-st.header(f'Landscape evolution: {year_1} vs. {year_2}')
 
-with col1:
-   st.header("Year 1")
-   st.image(image_1)
 
-with col2:
-   st.header("Year 2")
-   st.image(image_2)
+# class_1 = np.array(results['current_year']) # year_1 to be confirmed
+# class_2 = np.array(results['historical_year']) # year_2 to be confirmed
 
-with col3:
-   st.header("Landscape evolution")
-   st.dataframe(summary)
+# image_1 = get_new_image(adress, year_1)
+# image_2 = get_new_image(adress, year_2)
+# changes, summary = summary(class_1, class_2)
 
-col4, col5, col6 = st.columns(3)
+# col1, col2, col3 = st.columns(3)
 
-with col4:
-   st.image(class_1) #np.array: assign RGB code to each class
+# st.header(f'Landscape evolution: {year_1} vs. {year_2}')
 
-with col5:
-   st.image(class_2) #np.array: assign RGB code to each class
+# with col1:
+#    st.header("Year 1")
+#    st.image(image_1)
 
-with col6:
-   st.dataframe(changes)
+# with col2:
+#    st.header("Year 2")
+#    st.image(image_2)
+
+# with col3:
+#    st.header("Landscape evolution")
+#    st.dataframe(summary)
+
+# col4, col5, col6 = st.columns(3)
+
+# with col4:
+#    st.image(class_1) #np.array: assign RGB code to each class
+
+# with col5:
+#    st.image(class_2) #np.array: assign RGB code to each class
+
+# with col6:
+#    st.dataframe(changes)
