1ef69c974a734a357dd4424813eb0dd033ed80a9
Luis Henriques <luis.henriques@se.com>
2022-12-15 12:47:42 +0000

    test color

--- app.py
+++ app.py
@@ -106,112 +106,113 @@ params = dict(
 
 wfa_api_url = 'https://wfa04-tqv5zy4gla-ew.a.run.app/watchingfromabove/prediction'
 
-if submitted:
-    ##################################################
-    #          API Call                              #
-    ##################################################
-    response = requests.get(wfa_api_url, params=params)
-    results = response.json()
+with st.spinner('calling API'):
+    if submitted:
+        ##################################################
+        #          API Call                              #
+        ##################################################
+        response = requests.get(wfa_api_url, params=params)
+        results = response.json()
 
-    # Extract predictions for each image
-    cat_year_1_np = np.array(results['year_1']) # year_1 to be confirmed
-    cat_year_2_np = np.array(results['year_2']) # year_2 to be confirmed
+        # Extract predictions for each image
+        cat_year_1_np = np.array(results['year_1']) # year_1 to be confirmed
+        cat_year_2_np = np.array(results['year_2']) # year_2 to be confirmed
 
-    # using summary function to compare results
-    changes, sry = summary(cat_year_1_np, cat_year_2_np)
-
-
-    ##################################################
-    #          Getting images for display            #
-    ##################################################
-    # Get the correspondent images
-    image_year_1 = get_new_image(address, year_1)
-    image_year_2 = get_new_image(address, year_2)
+        # using summary function to compare results
+        changes, sry = summary(cat_year_1_np, cat_year_2_np)
 
 
-    ########################################################
-    #          FIRST TABLE - Plot original images          #
-    ########################################################
-    st.markdown("***")
-    colesp, col10, col11, col12, col13, col14, col15 = st.columns([0.4, 0.2, 0.85, 0.85, 0.85, 0.4, 0.2])
-    with col10:
-        st.subheader(f"{year_1}")
-
-    with col11:
-        st.image(image_year_1)
-
-    with col12:
-        img_changes_1 = landscape_changes(image_year_1, changes)
-        st.image(img_changes_1)
+        ##################################################
+        #          Getting images for display            #
+        ##################################################
+        # Get the correspondent images
+        image_year_1 = get_new_image(address, year_1)
+        image_year_2 = get_new_image(address, year_2)
 
 
-    with col13:
-        img_colormap_1 = image_colormap(cat_year_1_np)
-        st.image(img_colormap_1)
+        ########################################################
+        #          FIRST TABLE - Plot original images          #
+        ########################################################
+        st.markdown("***")
+        colesp, col10, col11, col12, col13, col14, col15 = st.columns([0.4, 0.2, 0.85, 0.85, 0.85, 0.4, 0.2])
+        with col10:
+            st.subheader(f"{year_1}")
+
+        with col11:
+            st.image(image_year_1)
+
+        with col12:
+            img_changes_1 = landscape_changes(image_year_1, changes)
+            st.image(img_changes_1)
 
 
-    with col14:
-        st.image(lbls)
-
-    #######################################################
-    #          SECOND TABLE - Plot comparision            #
-    #######################################################
-    colesp, col20, col21, col22, col23, col24, col25 = st.columns([0.4, 0.2, 0.85, 0.85, 0.85, 0.4, 0.2])
-    with col20:
-        st.subheader(f"{year_2}")
-
-    with col21:
-        st.image(image_year_2)
-
-    with col22:
-        img_changes_2 = landscape_changes(image_year_2, changes)
-        st.image(img_changes_2)
+        with col13:
+            img_colormap_1 = image_colormap(cat_year_1_np)
+            st.image(img_colormap_1)
 
 
-    with col23:
-        img_colormap_2 = image_colormap(cat_year_2_np)
-        st.image(img_colormap_2)
+        with col14:
+            st.image(lbls)
 
-    with col24:
-       pass
+        #######################################################
+        #          SECOND TABLE - Plot comparision            #
+        #######################################################
+        colesp, col20, col21, col22, col23, col24, col25 = st.columns([0.4, 0.2, 0.85, 0.85, 0.85, 0.4, 0.2])
+        with col20:
+            st.subheader(f"{year_2}")
+
+        with col21:
+            st.image(image_year_2)
+
+        with col22:
+            img_changes_2 = landscape_changes(image_year_2, changes)
+            st.image(img_changes_2)
 
 
-    #######################################################
-    #                       SUMMARY                       #
-    #######################################################
-    st.markdown(" ")
-    colt1, colt2, colt3 = st.columns([1, 1, 1])
-    with colt2:
-        columns_names = ["Categories", f"{year_1}", f"{year_2}", "Difference"]
-        sry.columns = columns_names
-        sry.set_index("Categories", inplace=True)
+        with col23:
+            img_colormap_2 = image_colormap(cat_year_2_np)
+            st.image(img_colormap_2)
 
-        ########## dataframe style
-        th_props = [
-        ('font-size', '20px'),
-        ('text-align', 'center'),
-        ('font-weight', 'bold'),
-        ('color', '#FFFFFF'),
-#        ('background-color', '#060537'),
-        ('border','1px solid #8e7cc3')
-        ]
+        with col24:
+        pass
 
-        td_props = [
-        ('font-size', '18px'),
-        ('text-align', 'center'),
-        ('color', '#FFFFFF'),
-#        ('background-color', '#FFFFFF'),
-        ('border','1px solid #8e7cc3')
-        ]
 
-        styles = [
-        dict(selector="th", props=th_props),
-        dict(selector="td", props=td_props)
-        ]
+        #######################################################
+        #                       SUMMARY                       #
+        #######################################################
+        st.markdown(" ")
+        colt1, colt2, colt3 = st.columns([1, 1, 1])
+        with colt2:
+            columns_names = ["Categories", f"{year_1}", f"{year_2}", "Difference"]
+            sry.columns = columns_names
+            sry.set_index("Categories", inplace=True)
 
-        # table
-        df2=sry.style.set_properties(**{'text-align':'center'}).set_table_styles(styles)
-        st.table(df2)
-        #st.dataframe(data=sry, width=700)
+            ########## dataframe style
+            th_props = [
+            ('font-size', '20px'),
+            ('text-align', 'center'),
+            ('font-weight', 'bold'),
+            ('color', '#FFFFFF'),
+    #        ('background-color', '#060537'),
+            ('border','1px solid #8e7cc3')
+            ]
 
-    st.balloons()
+            td_props = [
+            ('font-size', '18px'),
+            ('text-align', 'center'),
+            ('color', '#FFFFFF'),
+    #        ('background-color', '#FFFFFF'),
+            ('border','1px solid #8e7cc3')
+            ]
+
+            styles = [
+            dict(selector="th", props=th_props),
+            dict(selector="td", props=td_props)
+            ]
+
+            # table
+            df2=sry.style.set_properties(**{'text-align':'center'}).set_table_styles(styles)
+            st.table(df2)
+            #st.dataframe(data=sry, width=700)
+
+        st.balloons()
